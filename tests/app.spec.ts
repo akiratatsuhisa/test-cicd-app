f0597cd827849e4d9ce24a8e2a341adfc5b46e92
akiratatsuhisa <akiratatsuhisa@gmail.com>
2022-12-17 14:39:48 +0700

    fix CICD

--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -7,16 +7,18 @@ import app from "../src/app";
 use(chaiHttp);
 
 describe("Test Default", () => {
-  it("Call index", (done) => {
+  it("Call index", () => {
     request(app)
       .get("/")
-      .end((err, res) => {
+      .then((res) => {
         expect(res).have.status(200);
         expect(res.body).have.be.a("object");
         expect(res.body).have.property("message");
         expect(res.body.message).to.eq("Hello world!");
-
-        done();
       });
+  });
+
+  it("Call func", () => {
+    expect(10).have.to.eq(10);
   });
 });
